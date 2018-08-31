63528c600ca9313b2fab861f2ac1935b1986c5b7
Devin Miller <devinmiller@gmail.com>
2018-08-30 21:17:50 -0400

    added function to add to inventory

--- ch5/inventory.py
+++ ch5/inventory.py
@@ -1,4 +1,5 @@
 inventory = {'gold': 100, 'torch': 6, 'rope': 2, 'dagger': 1, 'arrow': 15}
+dragon_loot = ['gold', 'ruby', 'shield', 'gold', 'dagger', 'gold']
 
 def print_inventory(stuff):
     item_total = 0
@@ -10,4 +11,14 @@ def print_inventory(stuff):
     
     print(f"Total items in inventory: {item_total}")
 
+def add_inventory(stuff, loot):
+    for item in loot:
+        if item in stuff.keys():
+            stuff[item] += 1
+        else:
+            stuff.setdefault(item, 1)
+    return stuff
+
+print_inventory(inventory)
+inventory = add_inventory(inventory, dragon_loot)
 print_inventory(inventory)
