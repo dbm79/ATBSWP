c89932ab2413ad6d038c83705dcf8f3a7eaff7ea
Devin Miller <dbm79@millerha.us>
2018-09-09 14:53:48 -0400

    formatting

--- ch7/mad-libs.py
+++ ch7/mad-libs.py
@@ -3,79 +3,91 @@
 
 import sys, re
 
+
 def print_usage():
-    ''' This function will print the usage information if too few command-line arguments are given '''
-    print(f'Usage:')
-    print(f'python mad-libs.py <filename> - Reads the contents of <filename> for keywords to replace Mad-Libs style.')
+    """ This function will print the usage information if too few command-line arguments are given """
+    print(f"Usage:")
+    print(
+        f"python mad-libs.py <filename> - Reads the contents of <filename> for keywords to replace Mad-Libs style."
+    )
+
 
 def get_contents(filename):
-    ''' This function will read the input file contents in chunks and then return a list of the words in the file
+    """ This function will read the input file contents in chunks and then return a list of the words in the file
 
         Argument: name of file to open and read from
         Returns: List of words in the file
-    '''
+    """
 
     try:
-        with open(filename, 'r') as f:
-            
+        with open(filename, "r") as f:
+
             size_to_read = 10
             chunk = f.read(size_to_read)
             lines = chunk
-            
+
             while len(chunk) > 0:
                 chunk = f.read(size_to_read)
                 lines += chunk
-            
-            return lines.split(' ')
-    
+
+            return lines.split(" ")
+
     except EnvironmentError:
-        print(f'Unable to open file {filename}')
+        print(f"Unable to open file {filename}")
+
 
 def replace_items(contents):
-    ''' Take a list of strings and replace the string if it matches the regex NOUN, VERB, ADJECTIVE
+    """ Take a list of strings and replace the string if it matches the regex NOUN, VERB, ADJECTIVE
         Argument: list of strings
         Returns: List of strings
-    '''
+    """
 
     # Use regex to find NOUN, VERB, or ADJECTIVE
-    regex = re.compile(r'ADJECTIVE|NOUN|VERB')
+    regex = re.compile(r"ADJECTIVE|NOUN|VERB")
 
     for i in range(len(contents)):
         match = regex.match(contents[i])
-        
+
         # If a match is found ask user to replace it
         if match:
             to_replace = match.group(0)
-            contents[i] = regex.sub(input(f'Please enter a {to_replace.lower()}: '), contents[i])
-    
+            contents[i] = regex.sub(
+                input(f"Please enter a {to_replace.lower()}: "), contents[i]
+            )
+
     return contents
 
+
 def print_results(contents):
-    ''' Print a joined list contents to the screen
+    """ Print a joined list contents to the screen
         Argument: a list of strings
-    '''
-    print(' '.join(contents))
+    """
+    print(" ".join(contents))
+
 
 def write_results(contents):
-    ''' Write contents to a file named results.txt
+    """ Write contents to a file named results.txt
         Argument: a list of strings
-    '''
+    """
 
     try:
-        with open('results.txt', 'w') as wf:
-            to_write = ' '.join(contents)
+        with open("results.txt", "w") as wf:
+            to_write = " ".join(contents)
             wf.write(to_write)
-            print(f'Contents written to results.txt successfully')
+            print(f"Contents written to results.txt successfully")
     except EnvironmentError:
-        print(f'Unable to write to file.')    
+        print(f"Unable to write to file.")
+
 
 if len(sys.argv) <= 1:
     print_usage()
 else:
-    contents = get_contents(sys.argv[1])        # Get the contents of the file
-       
-    contents = replace_items(contents)          # Search contents for keywords and return replacements
+    contents = get_contents(sys.argv[1])  # Get the contents of the file
 
-    print_results(contents)                     # Print the results to the screen
+    contents = replace_items(
+        contents
+    )  # Search contents for keywords and return replacements
 
-    write_results(contents)                     # Write the results to a file
+    print_results(contents)  # Print the results to the screen
+
+    write_results(contents)  # Write the results to a file
--- ch7/mcw.py
+++ ch7/mcw.py
@@ -7,83 +7,93 @@
 
 import shelve, sys, pyperclip
 
-def print_usage():
-    ''' Print the usage information of the the program '''
 
-    print('''
+def print_usage():
+    """ Print the usage information of the the program """
+
+    print(
+        """
     Usage:
     python mcb.py save <keyword> - Saves clipboard to keyword
     python mcb.py <keyword> - Loads keyword data to clipboard
     python mcb.py delete <keyword> - Deletes the data associated with keyword
     python mcb.py delete-all <keyword> - Deletes all data
-    python mcb.py list - Lists all of the stored keywords''')
+    python mcb.py list - Lists all of the stored keywords"""
+    )
+
 
 def save_entry(database):
-    ''' Save data from the clipboard with the keywoard listed from sys.argv '''
-    ''' Param1 database: expects an open shelve object '''
+    """ Save data from the clipboard with the keywoard listed from sys.argv """
+    """ Param1 database: expects an open shelve object """
 
     database[sys.argv[2].lower()] = pyperclip.paste()
 
+
 def list_keys(database):
-    ''' List all of the keys from the opened shelve object '''
-    ''' Param1 database: expects an open shelve object '''
-    
-    print(f'List of Stored Keys:')
-    
+    """ List all of the keys from the opened shelve object """
+    """ Param1 database: expects an open shelve object """
+
+    print(f"List of Stored Keys:")
+
     for key in database.keys():
-        print(f'- {key}')
+        print(f"- {key}")
+
 
 def fetch_data(database):
-    ''' Take the sys.argv, try to find its key in the database, print the value and copy to clipboard '''
-    ''' Param1 database: expects an open shelve object '''
+    """ Take the sys.argv, try to find its key in the database, print the value and copy to clipboard """
+    """ Param1 database: expects an open shelve object """
 
     try:
-        print( database[sys.argv[1].lower()] )
-        pyperclip.copy( database[sys.argv[1].lower()] )
+        print(database[sys.argv[1].lower()])
+        pyperclip.copy(database[sys.argv[1].lower()])
     except KeyError:
-        print('Entry not found in database')
+        print("Entry not found in database")
+
 
 def delete_entry(database):
-    ''' Take the sys.argv, try to find its key in the database, print the value and copy to clipboard '''
-    ''' Param1 database: expects an open shelve object '''
+    """ Take the sys.argv, try to find its key in the database, print the value and copy to clipboard """
+    """ Param1 database: expects an open shelve object """
 
     try:
         del database[sys.argv[2].lower()]
-        print(f' Entry: {sys.argv[2].lower()} deleted.')
+        print(f" Entry: {sys.argv[2].lower()} deleted.")
     except KeyError:
-        print(f'Unable to delete entry')
+        print(f"Unable to delete entry")
+
 
 def delete_all(database):
-    ''' Deletes all data from the database '''
-    ''' Param1 database: expects an open shelve object '''
+    """ Deletes all data from the database """
+    """ Param1 database: expects an open shelve object """
 
     try:
         for key in database.keys():
-            print(f'Deleting: {key}')
+            print(f"Deleting: {key}")
             del database[key]
     except:
-        print(f'No keys to delete or other error')
+        print(f"No keys to delete or other error")
+
 
 try:
-    with shelve.open('mcb.db') as mcb_db:
-        
+    with shelve.open("mcb.db") as mcb_db:
+
         if len(sys.argv) <= 1:
             print_usage()
 
-        elif len(sys.argv) == 3 and sys.argv[1].lower() == 'save':
+        elif len(sys.argv) == 3 and sys.argv[1].lower() == "save":
             save_entry(mcb_db)
 
-        elif len(sys.argv) == 3 and sys.argv[1].lower() == 'delete':
+        elif len(sys.argv) == 3 and sys.argv[1].lower() == "delete":
             delete_entry(mcb_db)
 
         elif len(sys.argv) == 2:
-            
-            if sys.argv[1].lower() == 'list':
+
+            if sys.argv[1].lower() == "list":
                 list_keys(mcb_db)
-            elif sys.argv[1].lower() == 'delete-all':
+            elif sys.argv[1].lower() == "delete-all":
                 delete_all(mcb_db)
             else:
-                fetch_data(mcb_db)            
+                fetch_data(mcb_db)
 
 except EnvironmentError:
-    print(f'File cannot be accessed')
+    print(f"File cannot be accessed")
+
