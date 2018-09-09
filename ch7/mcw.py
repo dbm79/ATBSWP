2c06c7da7337b9fef54ff160314d5372a0a586cd
Devin Miller <dbm79@millerha.us>
2018-09-09 16:16:42 -0400

    added paramaters to all funcions that used sys.argv instead of using sys.argv directly

--- ch7/mcw.py
+++ ch7/mcw.py
@@ -18,20 +18,24 @@ def print_usage():
     python mcb.py <keyword> - Loads keyword data to clipboard
     python mcb.py delete <keyword> - Deletes the data associated with keyword
     python mcb.py delete-all <keyword> - Deletes all data
-    python mcb.py list - Lists all of the stored keywords"""
+    python mcb.py list - Lists all of the stored keywords
+    """
     )
 
 
-def save_entry(database):
-    """ Save data from the clipboard with the keywoard listed from sys.argv """
-    """ Param1 database: expects an open shelve object """
+def save_entry(database, entry_name):
+    """ Save data from the clipboard with the keywoard listed from sys.argv
+        Param1: expects an open shelve object
+        Param2: The name to save the entry as
+    """
 
-    database[sys.argv[2].lower()] = pyperclip.paste()
+    database[entry_name.lower()] = pyperclip.paste()
 
 
 def list_keys(database):
-    """ List all of the keys from the opened shelve object """
-    """ Param1 database: expects an open shelve object """
+    """ List all of the keys from the opened shelve object
+        Param1: expects an open shelve object
+    """
 
     print(f"List of Stored Keys:")
 
@@ -39,31 +43,36 @@ def list_keys(database):
         print(f"- {key}")
 
 
-def fetch_data(database):
-    """ Take the sys.argv, try to find its key in the database, print the value and copy to clipboard """
-    """ Param1 database: expects an open shelve object """
+def fetch_data(database, entry_name):
+    """ Try to find key in the database, print the value and copy to clipboard
+        Param1: expects an open shelve object
+        Param2: string to search keys for
+    """
 
     try:
-        print(database[sys.argv[1].lower()])
-        pyperclip.copy(database[sys.argv[1].lower()])
+        print(database[entry_name.lower()])
+        pyperclip.copy(database[entry_name.lower()])
     except KeyError:
         print("Entry not found in database")
 
 
-def delete_entry(database):
-    """ Take the sys.argv, try to find its key in the database, print the value and copy to clipboard """
-    """ Param1 database: expects an open shelve object """
+def delete_entry(database, entry_name):
+    """ Take the sys.argv, try to find its key in the database, print the value and copy to clipboard
+        Param1: expects an open shelve object
+        Param2: string name of entry to delete
+    """
 
     try:
-        del database[sys.argv[2].lower()]
-        print(f" Entry: {sys.argv[2].lower()} deleted.")
+        del database[entry_name.lower()]
+        print(f" Entry: {entry_name.lower()} deleted.")
     except KeyError:
         print(f"Unable to delete entry")
 
 
 def delete_all(database):
-    """ Deletes all data from the database """
-    """ Param1 database: expects an open shelve object """
+    """ Deletes all data from the database
+        Param1: database: expects an open shelve object
+    """
 
     try:
         for key in database.keys():
@@ -80,10 +89,10 @@ try:
             print_usage()
 
         elif len(sys.argv) == 3 and sys.argv[1].lower() == "save":
-            save_entry(mcb_db)
+            save_entry(mcb_db, sys.argv[2])
 
         elif len(sys.argv) == 3 and sys.argv[1].lower() == "delete":
-            delete_entry(mcb_db)
+            delete_entry(mcb_db, sys.argv[2])
 
         elif len(sys.argv) == 2:
 
@@ -92,7 +101,7 @@ try:
             elif sys.argv[1].lower() == "delete-all":
                 delete_all(mcb_db)
             else:
-                fetch_data(mcb_db)
+                fetch_data(mcb_db, sys.argv[1])
 
 except EnvironmentError:
     print(f"File cannot be accessed")
